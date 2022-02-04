4c71fd1bc65ea4f952d6520b4c1f93bf67b41238
insane4l <43768644+insane4l@users.noreply.github.com>
2022-02-04 20:39:00 +0200

    Homework 1. Message component

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
+import style from './Message.module.css'
 
-function Message() {
+function Message(props: MessagePropsType): ReactElement {
+
+    const {avatar, name, message, time} = props
+
     return (
-        <div>
+        <div className={style.wrapper}>
+            <div className={style.avatar}> 
+                <img className={style.image} src={avatar || 'https://i.pinimg.com/originals/13/a4/11/13a411076cdee39085cad97da215d9be.png'} alt="user_image" />
+            </div>
 
+            <div className={style.message}>
+                <div className={style.name}> {name} </div>
+                <div className={style.text}> {message} </div>
+                <div className={style.date}> {time} </div>
+            </div>
         </div>
     )
 }
 
 export default Message
+
+
+type MessagePropsType = {
+    avatar?: string
+    name: string
+    message: string
+    time: string
+}
